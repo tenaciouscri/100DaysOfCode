a8f63f0a2bc3fbca5524af600292f92bb2bcc0d3
tenaciouscri <cridelve@gmail.com>
2022-01-05 16:45:40 +0100

    Day 1

---
+++ 1-Beginner/Day1_exercises.py
@@ -0,0 +1,46 @@
+#! /urs/bin/env python3
+
+# 1. PRINTING
+
+# Write a program in main.py that prints the same notes
+# from the previous lesson using what you have learnt
+# about the Python print function.
+
+print("Day 1 - Python Print Function")
+print("The function is declared like this:")
+print("print('what to print')")
+
+# 2. DEBUGGING PRACTICE
+
+# Fix the code so that it runs without errors.
+
+print("Day 1 - String Manipulation")
+print('String Concatenation is done with the "+" sign.')
+print('e.g. print("Hello " + "world")')
+print("New lines can be created with a backslash and n.")
+
+# 3. INPUT FUNCTION
+
+# Write a program that prints the number of characters
+# in a user's name.
+
+print(len(input("What is your name? ")))
+
+# 4. VARIABLES
+
+# Write a program that switches the values stored in variables
+# a and b.
+
+# DO NOT EDIT
+a = input("a: ")
+b = input("b: ")
+# DO NOT EDIT
+
+c = a
+a = b
+b = c
+
+# DO NOT EDIT
+print("a: " + a)
+print("b: " + b)
+# DO NOT EDIT
