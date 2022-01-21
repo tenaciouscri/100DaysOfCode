49c7b31298cb811fda6ca01092e642e2d0dc7ad5
tenaciouscri <cridelve@gmail.com>
2022-01-21 16:11:35 +0100

    WIP Day 15

---
+++ 2-Intermediate/Day15/Day15_project.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python3
+
+# COFFEE MACHINE
+
+# TODO - Prompt user by asking what they'd like
+# Check user input to decide what to do next
+# Prompt should show every time an action is completed, to server the next customer
+user_choice = input("What would you like? (espresso/latte/cappuccino): ")
+
+# TODO - Turn of coffee machine by entering off
+# Execution ends when this happens
+if user_choice == "off":
+    pass
+# TODO - Print report
+
+# TODO - Check resources sufficient?
+
+# TODO - Process coins
+
+# TODO - Check transaction successful?
+
+# TODO - Make coffee
